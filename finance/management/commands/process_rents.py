b9e7ac9b71b7c6a01a93e6fafa0575dfbeb5e689
Mubaraka <carrivemubaraka28@gmail.com>
2026-02-18 16:51:50 +0100

    suppression du dossier finance/commande

--- finance/management/commands/process_rents.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from finance.models import Rent  # ton modèle Rent
-
-class Command(BaseCommand):
-    help = "Process and generate monthly rents"
-
-    def handle(self, *args, **kwargs):
-        Rent.process_rents()
-        self.stdout.write(self.style.SUCCESS("Rents processed successfully"))
